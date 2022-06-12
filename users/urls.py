09415e25aa44f91d8f73c991e256992b7b60efe3
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-12 18:12:57 +0300

    style: structure and style the signup/login page

--- users/urls.py
+++ users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('signup/', views.signup, name='users-signup')
+    path('signup/', views.signup, name='users-signup'),
+    path('signin/', views.signin, name='users-signin')
 ]
--- users/views.py
+++ users/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def signup(request):
     return render(request, 'users/signup.html')
+
+
+def signin(request):
+    return render(request, 'users/login.html')
