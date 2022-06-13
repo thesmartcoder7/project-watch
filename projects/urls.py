08f878d9f614bd37f2cae26819f8f0396fba0780
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-13 06:10:50 +0300

    style: stucture and style the profile page

--- projects/urls.py
+++ projects/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='projects_home' )
+    path('', views.home, name='projects-home' ),
+    path('profile/', views.profile, name='projects-profile')
 ]
--- projects/views.py
+++ projects/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home (request):
     return render(request, 'projects/index.html')
+
+def profile(request):
+    return render(request, 'projects/profile.html')
