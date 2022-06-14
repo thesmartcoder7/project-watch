8cfc7543d7fbc7c38a1d9c8a8d796c3e9790881e
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-14 15:29:17 +0300

    feature: streamline the user creation process

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name='users-signup'),
-    path('signin/', views.signin, name='users-signin')
+    path('signin/', auth_views.LoginView.as_view(template_name='users/login.html'), name='users-signin'),
+    path('signout/', auth_views.LogoutView.as_view(), name='users-signout'),
 ]
