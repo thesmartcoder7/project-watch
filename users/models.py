78efc6a1e32b8818ea631b903e5d6f90f5482a90
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-14 05:29:42 +0300

    feature: add user creation feature

--- users/models.py
+++ users/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None)
     image = models.ImageField(default='default.png', upload_to='project_watch/user_profiles/', blank=True)
     bio = models.TextField()
     github = models.CharField(max_length=2000, blank=True)
