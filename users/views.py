8ba877538b67fbe6c492ae5f76b18e3aa6b6d8b8
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-14 04:44:32 +0300

    feature: add user creation feature

--- users/views.py
+++ users/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render,  redirect
+from .forms import SignUpForm
+
 
 # Create your views here.
 def signup(request):
-    return render(request, 'users/signup.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users-signin')
+        else:
+            return render(request, 'users/signup.html', {'form': form})
+    else:
+        form = SignUpForm()
+        return render(request, 'users/signup.html', {'form': form})
 
 
 def signin(request):
