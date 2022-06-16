283353eb7b437e9d620766001d1bf70cc7dace18
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-17 00:01:55 +0300

    release: fix psycopg2 error

--- project_watch/settings.py
+++ project_watch/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'project_watch.urls'
@@ -88,8 +89,6 @@ DATABASES = {
         'PASSWORD': os.getenv('DATABASE_PASSWORD')
     }
 }
-
-DATABASE_PORT = '5433'
 
 
 # Password validation
