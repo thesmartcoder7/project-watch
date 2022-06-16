ef98d9b08ede179b6cfccccfd158309cad7564e6
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-16 23:09:53 +0300

    release: fix psycopg2 error

--- project_watch/settings.py
+++ project_watch/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'project_watch.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DATABASE_NAME'),
         'USERNAME': os.getenv('DATABASE_USERNAME'),
         'PASSWORD': os.getenv('DATABASE_PASSWORD')
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
